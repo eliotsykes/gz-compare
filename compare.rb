cc338f16b98a2910cb0f4964075bfe5b441306ff
Eliot Sykes <eliotsykes@gmail.com>
2015-12-06 14:11:14 +0000

    Percentage of file size comparison

--- compare.rb
+++ compare.rb
@@ -21,11 +21,10 @@ def zopfli_compress(source, source_file_name, num_iterations=nil)
   output_file_name
 end
 
-def assert_decompresses(compressed_file_name, expected_uncompressed_data)
-  compressed_file = File.open(compressed_file_name)
+def assert_decompresses(compressed_file, expected_uncompressed_data)
   uncompressed_data = Zlib::GzipReader.new(compressed_file).read
   if uncompressed_data != expected_uncompressed_data
-    raise "#{compressed_file_name} did not decompress to expected data"
+    raise "#{compressed_file.path} did not decompress to expected data"
   end
 end
 
@@ -38,12 +37,21 @@ source = File.read(source_file_name)
 gzipped_file_name = gzip_compress(source, source_file_name)
 zopflied_file_name = zopfli_compress(source, source_file_name)
 
-puts "Original file size: #{File.size(source_file_name)}"
-puts " Zlibbed file size: #{File.size(gzipped_file_name)}"
-puts "Zopflied file size: #{File.size(zopflied_file_name)}"
+gzipped_file = File.new(gzipped_file_name)
+zopflied_file = File.new(zopflied_file_name)
 
-assert_decompresses(gzipped_file_name, source)
-assert_decompresses(zopflied_file_name, source)
+assert_decompresses(gzipped_file, source)
+assert_decompresses(zopflied_file, source)
+
+def compare_file_size(file1, file2)
+  percentage = file1.size/file2.size.to_f * 100
+  "#{percentage.round(1)}% of #{file2.path}"
+end
+
+# puts "Original file size: #{File.size(source_file_name)}"
+puts " Gzipped file size: #{gzipped_file.size}"
+puts "Zopflied file size: #{zopflied_file.size} (#{compare_file_size(zopflied_file, gzipped_file)})"
+
 
 # TODO:
 # - compare file sizes for JS, CSS, SVG examples
